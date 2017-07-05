750eae82e061f74d722eb9abbf87447a73a7195e
Alby Thomas <Alby@Albys-MacBook-Pro.local>
2017-07-04 21:46:21 -0400

    str

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def process():
 	client = textapi.Client("691d60ef", "e84ff6dca64ad756bd7676e6aef7f989")
 	words = request.form['text']
 	number = request.form['count']
-	sentiment = client.Sentiment({'text': words})
-	summary = client.Summarize({'title': "Hello World",'text': words, 'sentences_number': int(number)})
-	classifications = client.Classify({"text": words})
+	sentiment = client.Sentiment({'text': str(words)})
+	summary = client.Summarize({'title': "Hello World",'text': str(words), 'sentences_number': int(number)})
+	classifications = client.Classify({"text": str(words)})
 	return render_template('index.html',sentiment=sentiment['polarity'],summary=summary['sentences'],classification=classifications['categories'][0]['label'])
 
 if __name__ == '__main__':
