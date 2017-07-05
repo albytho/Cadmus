534c309577afc575f49287beffcea02c341ee8ba
Alby Thomas <Alby@Albys-MacBook-Pro.local>
2017-07-05 17:51:44 -0400

    error log

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, flash, render_template, request
 from aylienapiclient import textapi
+from logging import FileHandler, WARNING
 
 app = Flask(__name__)
+
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
 
 @app.route('/')
 def index():
